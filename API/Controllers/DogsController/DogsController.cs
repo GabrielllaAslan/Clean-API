25648b92808b909dba0fde3539c35916a4492127
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2023-12-04 16:22:21 +0100

    Validation for dog

--- API/Controllers/DogsController/DogsController.cs
+++ API/Controllers/DogsController/DogsController.cs
@@ -3,6 +3,9 @@ using Application.Commands.Dogs.UpdateDog;
 using Application.Dtos;
 using Application.Queries.Dogs.GetAll;
 using Application.Queries.Dogs.GetById;
+using Application.Validators;
+using Application.Validators.Dog;
+using FluentValidation.TestHelper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,9 +18,14 @@ namespace API.Controllers.DogsController
     public class DogsController : ControllerBase
     {
         internal readonly IMediator _mediator;
-        public DogsController(IMediator mediator)
+        internal readonly DogValidator _dogvalidator;
+        internal readonly GuidValidator _guidvalidator;
+        public DogsController(IMediator mediator,DogValidator dogvalidator,GuidValidator guidvalidator)
         {
             _mediator = mediator;
+            _dogvalidator = dogvalidator;
+            _guidvalidator = guidvalidator;
+            
         }
 
         // Get all dogs from database
@@ -34,6 +42,14 @@ namespace API.Controllers.DogsController
         [Route("getDogById/{dogId}")]
         public async Task<IActionResult> GetDogById(Guid dogId)
         {
+            var guidvalidator = _guidvalidator.Validate(dogId);
+            
+            if(! guidvalidator.IsValid)
+            {
+
+                return BadRequest(guidvalidator.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+            
             return Ok(await _mediator.Send(new GetDogByIdQuery(dogId)));
         }
 
@@ -42,18 +58,57 @@ namespace API.Controllers.DogsController
         [Route("addNewDog")]
         public async Task<IActionResult> AddDog([FromBody] DogDto newDog)
         {
-            return Ok(await _mediator.Send(new AddDogCommand(newDog)));
+            ///Validate dog
+            var dogvalidator = _dogvalidator.Validate(newDog);
+           
+            // Error handling
+            if(!dogvalidator.IsValid)
+            {
+                return BadRequest(dogvalidator.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+            //Try,Catch
+            try
+            {
+                return Ok(await _mediator.Send(new AddDogCommand(newDog)));
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
         }
 
         // Update a specific dog
         [HttpPut]
         [Route("updateDog/{updatedDogId}")]
-        public async Task<IActionResult> UpdateDog([FromBody] DogDto updatedDog, Guid updatedDogId)
+        public async Task<IActionResult> UpdateDog([FromBody] DogDto dogToUpdate, Guid updatedDogById)
         {
-            return Ok(await _mediator.Send(new UpdateDogByIdCommand(updatedDog, updatedDogId)));
+
+            var dogvalidator = _dogvalidator.Validate(dogToUpdate);
+            var guidvalidator = _guidvalidator.Validate(updatedDogById);
+
+            if(!guidvalidator.IsValid) 
+            {
+                return BadRequest(guidvalidator.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+
+            if(!dogvalidator.IsValid)
+            {
+                return BadRequest(dogvalidator.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+            var dog = await _mediator.Send(new UpdateDogByIdCommand(dogToUpdate, updatedDogById));
+            
+            if(dog == null)
+            {
+                return NotFound($"Dog with Id:{updatedDogById} does not exist in database");
+            }
+            return Ok(dog);
         }
 
         // IMPLEMENT DELETE !!!
-
+        // Retunera (return NoContent(); nrä jag skriver validering för delete)!
     }
 }
---
+++ Application/Validators/GuidValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators
+{
+    public class GuidValidator : AbstractValidator<Guid>
+    {
+         public GuidValidator() 
+        {
+            RuleFor(guid => guid).NotNull().WithMessage("Guid can not be null")
+                .NotEmpty().WithMessage("Guid can not be empty")
+                .NotEqual(Guid.Empty).WithMessage("Guid should ´not be empty");
+        }
+    }
+}
