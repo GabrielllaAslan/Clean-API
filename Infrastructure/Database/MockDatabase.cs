fa522dda0d1d4935af77b4e1b242994c66cc22c4
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2023-12-08 23:04:56 +0100

    Adding cat and Making validation for cats.

--- Infrastructure/Database/MockDatabase.cs
+++ Infrastructure/Database/MockDatabase.cs
@@ -17,5 +17,19 @@ namespace Infrastructure.Database
             new Dog { Id = Guid.NewGuid(), Name = "Alfred"},
             new Dog { Id = new Guid("12345678-1234-5678-1234-567812345678"), Name = "TestDogForUnitTests"}
         };
+        public List<Cat> Cats
+        {
+            get { return allCats; }
+            set { allCats = value; }
+        }
+
+        private static List<Cat> allCats = new()
+        {
+            new Cat { Id = Guid.NewGuid(), Name = "Bella", LikesToPlay = true },
+            new Cat { Id = Guid.NewGuid(), Name = "Fille", LikesToPlay = true},
+            new Cat { Id = Guid.NewGuid(), Name = "Kalle", LikesToPlay = true},
+            new Cat { Id = new Guid("12345678-1234-5678-1234-567812345610"), Name = "TestCatForUnitTests", LikesToPlay = true },
+            new Cat { Id = new Guid("12345678-1234-5678-1234-567812345609"), Name = "AnotherTestCatForUnitTests", LikesToPlay = false }
+        };
     }
 }
