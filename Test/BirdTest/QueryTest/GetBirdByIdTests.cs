60645d34e599c4616fe255688da7a3342847b075
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2023-12-10 15:44:23 +0100

    Adding query test for bird

--- Test/BirdTest/QueryTest/GetBirdByIdTests.cs
+++ Test/BirdTest/QueryTest/GetBirdByIdTests.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using Application.Queries.Birds.GetById;
+using Application.Queries.Cats.GetById;
+using Infrastructure.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test.BirdTest.QueryTest
+namespace Test.BirdTests.QueryTest
 {
-    internal class GetBirdByIdTests
+    [TestFixture]
+    public class GetBirdByIdTests
     {
+        private GetBirdByIdQueryHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Initialize the handler and mock database before each test
+            _mockDatabase = new MockDatabase();
+            _handler = new GetBirdByIdQueryHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_ValidId_ReturnCorrectBird()
+        {
+            //Arrange
+            var birdId = new Guid("12345678-1234-5678-1234-567812345603");
+
+            var query = new GetBirdByIdQuery(birdId);
+
+            //Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.That(result.Id, Is.EqualTo(birdId));
+        }
+        [Test]
+        public async Task Handle_InvalidId_ReturnNull()
+        {
+            //Arrange
+            var invalidBirdId = Guid.NewGuid();
+
+            var query = new GetBirdByIdQuery(invalidBirdId);
+
+            //Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            //Assert
+            Assert.IsNull(result);
+        }
+
     }
 }
