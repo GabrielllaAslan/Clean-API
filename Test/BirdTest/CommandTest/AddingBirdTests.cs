7b537f3545a7a05a820431881c28a2a141b749a3
Gabrielllaaslan <gabriella.lahdo@student.nbi-handelsakademin.se>
2023-12-10 13:56:09 +0100

    Adding query test to bird.

--- Test/BirdTest/CommandTest/AddingBirdTests.cs
+++ Test/BirdTest/CommandTest/AddingBirdTests.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Application.Commands.Birds.AddBird;
+using Application.Dtos;
+using Infrastructure.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test.BirdTest.CommandTest
+namespace Test.BirdTests.CommandTest
 {
-    internal class AddingBirdTest
+    [TestFixture]
+    public class AddBirdTests
     {
+        private AddBirdCommandHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDatabase = new MockDatabase();
+            _handler = new AddBirdCommandHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_Add_Bird_To_MockDatabase()
+        {
+            //Arrange
+            var birdName = "Olle";
+
+            var dto = new BirdDto();
+
+            dto.Name = birdName;
+
+            dto.CanFly = false;
+
+            var command = new AddBirdCommand(dto);
+
+            //Act 
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
